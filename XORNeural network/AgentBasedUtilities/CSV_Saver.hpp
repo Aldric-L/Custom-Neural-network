8b9e528bd3d1c70458b94cbb1f4167b3943d6a83
Aldric-L <al9.labarthe@free.fr>
2023-11-07 13:48:29 +0100

    Update class akml::Save to avoid inheritance

--- XORNeural network/AgentBasedUtilities/CSV_Saver.hpp
+++ XORNeural network/AgentBasedUtilities/CSV_Saver.hpp
@@ -17,25 +17,32 @@
 
 namespace akml {
 
+    template <class SaveClass>
     class CSV_Saver {
     protected:
-        std::vector<std::pair<std::size_t, akml::Save*>> memory;
+        std::vector<std::pair<std::size_t, SaveClass*>> memory;
         
     public:
-        inline void addSave(akml::Save** iteration) {
-            memory.push_back(std::make_pair((memory.size() == 0) ? 1 : memory.back().first+1, *iteration));
+        inline void addSave(SaveClass* iteration) {
+            memory.push_back(std::make_pair((memory.size() == 0) ? 1 : memory.back().first+1, iteration));
         }
         
-        template <class Saveclass>
         inline void saveToCSV(const std::string filepathandname="data.csv") {
             std::ofstream file;
             file.open (filepathandname);
-            file << "iteration," << Saveclass::printTitleAsCSV() <<"\n";
+            file << "iteration," << memory[0].second->printTitleAsCSV() <<"\n";
             for (int it(0); it < memory.size(); it++){
                 file << it << "," << memory[it].second->printAsCSV() <<"\n";
             }
             file.close();
         }
+        
+        inline ~CSV_Saver() {
+            for (int it(0); it < memory.size(); it++){
+                delete memory[it].second;
+            }
+        }
+        
     };
 
 }
